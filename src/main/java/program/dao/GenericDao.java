1d5543e9b38e87168f0d77569c533cf6e3c9a027
sx-9155 <sx-wangyc@dtdream.com>
2016-12-09 20:37:21 +0800

    删除一个不用的属性。

--- src/main/java/program/dao/GenericDao.java
+++ src/main/java/program/dao/GenericDao.java
@@ -9,26 +9,25 @@ import java.util.List;
 import java.util.Map;
 @Repository(value="GenericDao")//这个value是bean的名字
 public class GenericDao extends BaseDao {
-    SingleClassSqlConstructor singleClassSqlConstructor=new SingleClassSqlConstructor();
     public <T> Integer simpleDelete(T condition){//返回通过一个查询条件【删除的列数】
         Pair<String, Map<String, Object>> deleteQuestionMarkSql_and =
-                singleClassSqlConstructor.createQuestionMarkSql_and(condition,SingleClassSqlConstructor.type_DELETE);
+                SingleClassSqlConstructor.createQuestionMarkSql_and(condition,SingleClassSqlConstructor.type_DELETE);
         Query query = getSession().createQuery(deleteQuestionMarkSql_and.getKey());
-        singleClassSqlConstructor.setParam(query,deleteQuestionMarkSql_and.getValue());
+        SingleClassSqlConstructor.setParam(query,deleteQuestionMarkSql_and.getValue());
         return query.executeUpdate();
     }
     public <T> T simpleQueryOne(T condition){
         Pair<String, Map<String, Object>> deleteQuestionMarkSql_and =
-                singleClassSqlConstructor.createQuestionMarkSql_and(condition,SingleClassSqlConstructor.type_QUERY);
+                SingleClassSqlConstructor.createQuestionMarkSql_and(condition,SingleClassSqlConstructor.type_QUERY);
         Query query = getSession().createQuery(deleteQuestionMarkSql_and.getKey());
-        singleClassSqlConstructor.setParam(query,deleteQuestionMarkSql_and.getValue());
+        SingleClassSqlConstructor.setParam(query,deleteQuestionMarkSql_and.getValue());
         return (T)query.list().get(0);
     }
     public <T> List<T> simpleQueryList(T condition){
         Pair<String, Map<String, Object>> deleteQuestionMarkSql_and =
-                singleClassSqlConstructor.createQuestionMarkSql_and(condition,SingleClassSqlConstructor.type_QUERY);
+                SingleClassSqlConstructor.createQuestionMarkSql_and(condition,SingleClassSqlConstructor.type_QUERY);
         Query query = getSession().createQuery(deleteQuestionMarkSql_and.getKey());
-        singleClassSqlConstructor.setParam(query,deleteQuestionMarkSql_and.getValue());
+        SingleClassSqlConstructor.setParam(query,deleteQuestionMarkSql_and.getValue());
         return query.list();
     }
     public <T> List<T> simpleGetAll(Class<?> clazz){
