82861b5485735359f9612dde5952542df95a0682
sx-9155 <sx-wangyc@dtdream.com>
2016-12-14 18:18:10 +0800

    加了一行注释

--- src/main/webapp/view/2angular_component_Js/directiveRollPicture.js
+++ src/main/webapp/view/2angular_component_Js/directiveRollPicture.js
@@ -4,6 +4,7 @@ app.directive('directiveRollPicture', function () {
         templateUrl: directiveHtmlUrl+'directiveRollPicture.html',
         replace: true,
         controller:function ($scope,$http) {
+            //当在controller中指定了controller的名字，或者函数，则在对应的html页面中，则不需要再使用ng-controller指令绑定controller
             $http.get(webRootUrl+"getRollPicture").then(function (data) {
                 $scope.pictures=data.data
             })
